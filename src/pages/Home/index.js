15d612545326336459fc60b1712d4e64b64dafaa
PedroVerano <pedroverano2009@yahoo.com.br>
2023-12-24 20:31:52 -0300

    Adicionando dependencias, Componentes e Map.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,37 @@
+import { Container, PokedexList, Pokemon} from "./styles";
+
 function Home () {
+    const pokemons = [
+        {
+            id:1,
+            name: 'Bulbassauro',
+            image_url: "https://pbs.twimg.com/profile_images/1225497788491931648/B6k9ZlCp_400x400.jpg"
+        },
+        {
+            id:2,
+            name: 'Ivysar',
+            image_url: "https://pbs.twimg.com/profile_images/1225497788491931648/B6k9ZlCp_400x400.jpg"
+        },
+        {
+            id:3,
+            name: 'Charmander',
+            image_url: "https://pbs.twimg.com/profile_images/1225497788491931648/B6k9ZlCp_400x400.jpg"
+        }
+    ]
     return(
-        <h1>Hello World</h1>
+        <Container>
+            <h1>Pokedex</h1>
+            <PokedexList>
+                {pokemons.map(pokemon => {
+                    return(
+                        <Pokemon key={pokemon.id}>
+                            <a href=""><img src={pokemon.image_url} alt={pokemon.name}></img></a>
+                            <span>{pokemon.name}</span>
+                        </Pokemon>
+                    )
+                })}
+            </PokedexList>
+        </Container>
     )
 }
 
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+h1 {
+    text-align: center;
+    margin: 4rem 0;
+}
+
+li:hover {
+    transform: scale(1.1);
+}
+`
+export const PokedexList = styled.ul`
+list-style: none;
+display: grid;
+grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+column-gap: 3rem;
+row-gap: 4rem;
+`
+
+export const Pokemon = styled.li`
+    width: 300px;
+    height: 200px;
+    background-color: darkgrey;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 2rem;
+    transition: all 0.3s;
+
+img {
+        width: 180px;
+    }
+`
+
+
+//export default Container;
